7cb74d51414572f7d0b4885bbd38d15c1036e4db
Jeremy <jpj054000@gmail.com>
2018-08-12 14:21:02 -0500

    working build with v0.1 of code, next need to run a test

--- src/main/java/org/example/basicapp/App.java
+++ src/main/java/org/example/basicapp/App.java
@@ -1,5 +1,20 @@
 package org.example.basicapp;
 
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.services.cloudformation.AmazonCloudFormation;
+import com.amazonaws.services.cloudformation.model.DescribeStackResourcesRequest;
+import com.amazonaws.services.cloudformation.model.DescribeStacksRequest;
+import com.amazonaws.services.cloudformation.model.Stack;
+import com.amazonaws.services.cloudformation.model.StackResource;
+import com.amazonaws.services.cloudformation.model.StackStatus;
+import com.amazonaws.services.cloudformation.AmazonCloudFormationClientBuilder;
+import com.amazonaws.services.cloudformation.model.*;
+import com.amazonaws.regions.Regions;
+import java.util.List;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 /**
  * Hello world!
  *
@@ -9,5 +24,28 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public String myHandler(String stackName, Context context) {
+        LambdaLogger logger = context.getLogger();
+
+        final AmazonCloudFormation cfn = AmazonCloudFormationClientBuilder.standard()
+          .withRegion(Regions.US_EAST_1)
+          .build();
+
+        Stack myStack = null;
+
+        List<Stack> stacks = cfn.describeStacks(new DescribeStacksRequest()
+          .withStackName(stackName))
+          .getStacks();
+
+        myStack = stacks.get(0);
+
+        String description = myStack.getDescription();
+        Boolean bTermationProtection = myStack.getEnableTerminationProtection();
+        logger.log("Description: " + description);
+        logger.log("Protection:  " + bTermationProtection);
+        String result = "Termination protection for " + stackName + " is " + bTermationProtection;
+        return result;
     }
 }
